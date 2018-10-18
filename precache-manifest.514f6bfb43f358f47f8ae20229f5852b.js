7c8f43f464a776c41ed36329a1f192144ec7bd16
joe <jchimien@gmail.com>
2018-10-18 12:55:37 -0400

    Updates

---
+++ precache-manifest.514f6bfb43f358f47f8ae20229f5852b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7876d48e80112e766d737346a616707",
+    "url": "/static/media/btc.d7876d48.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2bac01ed8eab95aabf10",
+    "url": "/static/js/main.2bac01ed.chunk.js"
+  },
+  {
+    "revision": "c3fecacd445adc544374",
+    "url": "/static/js/1.c3fecacd.chunk.js"
+  },
+  {
+    "revision": "2bac01ed8eab95aabf10",
+    "url": "/static/css/main.f4df630e.chunk.css"
+  },
+  {
+    "revision": "c3fecacd445adc544374",
+    "url": "/static/css/1.6b85740c.chunk.css"
+  },
+  {
+    "revision": "5ecbef850da510fe60e5bcaa27088c18",
+    "url": "/index.html"
+  }
+];
