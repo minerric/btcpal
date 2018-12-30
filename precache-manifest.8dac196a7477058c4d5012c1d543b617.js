394f450df68e2d08b54132a3abf00c93203be847
joe <jchimien@gmail.com>
2018-12-30 12:39:41 -0500

    Updates

---
+++ precache-manifest.8dac196a7477058c4d5012c1d543b617.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "600a102a19efe9e1dcb8",
+    "url": "/static/css/main.3c504b88.chunk.css"
+  },
+  {
+    "revision": "600a102a19efe9e1dcb8",
+    "url": "/static/js/main.600a102a.chunk.js"
+  },
+  {
+    "revision": "68600b9fe050d2631252",
+    "url": "/static/css/1.adf9f551.chunk.css"
+  },
+  {
+    "revision": "68600b9fe050d2631252",
+    "url": "/static/js/1.68600b9f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d7876d48e80112e766d737346a616707",
+    "url": "/static/media/btc.d7876d48.svg"
+  },
+  {
+    "revision": "01aad15dc02b29842f6086244dc438be",
+    "url": "/static/media/lightning-node.01aad15d.svg"
+  },
+  {
+    "revision": "df2209cb51eef2890fd417c8daffdc70",
+    "url": "/static/media/checkout.df2209cb.png"
+  },
+  {
+    "revision": "ebaebbf7430f60da5bdd4d10d684549b",
+    "url": "/static/media/invoice.ebaebbf7.png"
+  },
+  {
+    "revision": "5907a0163f095bdc483b8798f8770a6c",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.37ecd75ed9941a9ff238d8b95256ccd7.js"
+  "/precache-manifest.8dac196a7477058c4d5012c1d543b617.js"
 );
 
 workbox.clientsClaim();
